0aaf361fe804b4c0e1447d2d1946ecb5c67ff4e1
Jonathan Van Eenwyk <jonathan@vihanti.com>
2020-04-21 09:55:25 -0700

    Fix timestamp on public folder

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# Fix the timestamp on the public folder
+touch public
+
 # Go To Public folder
 cd public
 
